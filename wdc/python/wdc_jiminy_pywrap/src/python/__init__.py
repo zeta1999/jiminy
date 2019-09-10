b77b42d5410803c5ad50d7a01ddae8b98a79e99a
Grégoire Roussel <gregoire.roussel@wandercraft.eu>
2019-09-10 17:44:07 +0200

    [dev] better error suppression during imports

--- wdc/python/wdc_jiminy_pywrap/src/python/__init__.py
+++ wdc/python/wdc_jiminy_pywrap/src/python/__init__.py
@@ -7,17 +7,16 @@
 import os as _os # private import
 import sys as _sys
 
-if (_sys.version_info > (3, 0)):
+if _sys.version_info > (3, 0):
     from contextlib import redirect_stderr as _redirect_stderr
     with open(_os.devnull, 'w') as stderr, _redirect_stderr(stderr):
         from jiminy import *  # Load jiminy module since wdc_jiminy is built on top of it
         from .libwdc_jiminy_pywrap import *
 else:
     with open(_os.devnull, 'w') as stderr:
-        old_target = _sys.stderr
         _sys.stderr = stderr
-
-        from jiminy import *
-        from .libwdc_jiminy_pywrap import *
-
-        _sys.stderr = old_target
+        try:
+            from jiminy import *
+            from .libwdc_jiminy_pywrap import *
+        finally:
+            _sys.stderr = _sys.__stderr__
